1a47aa131d323c89e2b6dee4e917c271a0d45574
sameerjani09988776-source <sameerjani09988776@gmail.com>
2026-02-18 23:35:59 -0800

    Add files via upload

---
+++ class_5.py
@@ -0,0 +1,12 @@
+# num_1= int(input("enter first num"))
+# num_2= int(input("enter second num"))
+# print(num_1*num_2,num_1+num_2)
+num_1= int(input("enter first num: "))
+num_2= int (input("enter second num: "))
+print("sum",num_1+num_2)
+print("minas",num_1-num_2)
+print("division",num_1/num_2)
+
+
+
+
